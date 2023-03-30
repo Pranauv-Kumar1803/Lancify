40159c70c57efc61b889739aa58cd5e4ab644afa
siddharthav45 <siddhartha.v21@iiits.in>
2023-03-30 10:33:06 +0530

    added password authentication

--- public/js/login.js
+++ public/js/login.js
@@ -1,102 +1,112 @@
 /*eslint-disable*/
 
-const signup_form = document.getElementById('signup-form');
+const signup_form = document.getElementById("signup-form");
 
-signup_form.addEventListener('submit', async e => {
-    e.preventDefault();
-    const name = signup_form.querySelector('#name').value;
-    const email = signup_form.querySelector('#email').value;
-    const password = signup_form.querySelector('#password').value;
-    const confirmpassword = signup_form.querySelector('#confirmpassword').value;
+signup_form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = signup_form.querySelector("#name").value;
+  const email = signup_form.querySelector("#email").value;
+  const password = signup_form.querySelector("#password").value;
+  const confirmpassword = signup_form.querySelector("#confirmpassword").value;
 
-    if (name === "") {
-        alert("Name is required");
-        return;
-    }
+  if (name === "") {
+    alert("Name is required");
+    return;
+  }
 
-    if (!isValidEmail(email)) {
-        alert("Invalid email address");
-        return;
-    }
+  if (!isValidEmail(email)) {
+    alert("Invalid email address");
+    return;
+  }
 
-    if (password.length < 7) {
-        alert("Password must be at least 7 characters long.");
-        return;
-    }
+  if (password.length < 7) {
+    alert("Password must be at least 7 characters long.");
+    return;
+  }
 
-    if (password !== confirmpassword) {
-        alert("Passwords do not match");
-        return;
-    }
+  if (!isValidPassword(password)) {
+    alert(
+      "password should contain atleast one digit one special character ,one uppercase letter"
+    );
+    return;
+  }
 
-    obj = { name: name, email: email, password: password };
-    const res = await fetch('http://localhost:5500/register', {
-        method: 'post',
-        body: JSON.stringify(obj),
-        headers: {
-            "Content-Type": 'application/json'
-        }
-    });
+  if (password !== confirmpassword) {
+    alert("Passwords do not match");
+    return;
+  }
 
-    if (!res.ok) {
-        alert('invalid input/server error');
-        signup_form.reset();
-    }
-    else {
-        alert('Sign up successful');
-        window.location.href = '/login'
-    }
+  obj = { name: name, email: email, password: password };
+  const res = await fetch("http://localhost:5500/register", {
+    method: "post",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    const res1 = await fetch('http://localhost:5500/users');
-    if (res.ok) {
-    }
+  if (!res.ok) {
+    alert("invalid input/server error");
+    signup_form.reset();
+  } else {
+    alert("Sign up successful");
+    window.location.href = "/login";
+  }
 
-    if (!res.ok) {
-        alert('invalid input/server error');
-        signup_form.reset();
-    } else {
-        alert('Sign up successful');
-        window.location.href = '/login';
-    }
+  // const res1 = await fetch('http://localhost:5500/users');
+  // if (res.ok) {
+  // }
+
+  // if (!res.ok) {
+  //     alert('invalid input/server error');
+  //     signup_form.reset();
+  // } else {
+  //     alert('Sign up successful');
+  //     window.location.href = '/login';
+  // }
 });
 
-const signin_form = document.getElementById('signin-form');
+const signin_form = document.getElementById("signin-form");
 
-signin_form.addEventListener('submit', async e => {
-    e.preventDefault();
-    const email = signin_form.querySelector('#email').value;
-    const password = signin_form.querySelector('#password').value;
+signin_form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = signin_form.querySelector("#email").value;
+  const password = signin_form.querySelector("#password").value;
 
-    if (!isValidEmail(email)) {
-        alert("Invalid email address.");
-        return;
-    }
+  if (!isValidEmail(email)) {
+    alert("Invalid email address.");
+    return;
+  }
 
-    if (password === "") {
-        alert("Password is required.");
-        return;
-    }
+  if (password === "") {
+    alert("Password is required.");
+    return;
+  }
 
-    obj = { email: email, password: password };
-    const res = await fetch('http://localhost:5500/login', {
-        method: 'post',
-        body: JSON.stringify(obj),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  obj = { email: email, password: password };
+  const res = await fetch("http://localhost:5500/login", {
+    method: "post",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (!res.ok) {
-        alert('User Not Found / Server Error');
-        signin_form.reset();
-    }
-    else {
-        alert('login successful');
-        window.location.href = '/'
-    }
+  if (!res.ok) {
+    alert("User Not Found / Server Error");
+    signin_form.reset();
+  } else {
+    alert("login successful");
+    window.location.href = "/";
+  }
 });
 
 function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+  const passWordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]|\\:;"'<,>.?/])[A-Za-z\d!@#$%^&*()_+={}\[\]|\\:;"'<,>.?/]{7,}$/;
+  return passWordRegex.test(password);
 }
