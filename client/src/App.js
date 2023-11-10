fcca1d9e917716c65ffeb239c940aab561eb3716
siddharthav45 <siddhartha.v21@iiits.in>
2023-11-10 11:28:30 +0530

    done with page not found component

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,22 @@ import Home from "./components/Home";
 import Something from "./components/Something";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+import PageNotFound from "./components/auth/PageNotFound";
 
 function App() {
   return (
     <>
       <Routes>
-
         <Route path="/auth">
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
         </Route>
 
-        <Route path="/" >
+        <Route path="/">
           <Route index element={<Home />} />
           <Route path="something" element={<Something />} />
         </Route>
-
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
