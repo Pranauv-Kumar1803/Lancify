2621e10f504065ef1b96f5cc20f56d3947f800ad
siddharthav45 <siddhartha.v21@iiits.in>
2023-11-12 09:09:28 +0530

    add signup component

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
 const Login = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit()}>
+      <FormControl isInvalid={errors.name}>
+        <FormLabel htmlFor='name'>First name</FormLabel>
+        <Input
+          id='name'
+          placeholder='name'
+          {...register('name', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.name && errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme='teal' isLoading={true} type='submit'>
+        Submit
+      </Button>
+    </form>
   )
 }
 
