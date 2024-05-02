33b2ed7dddb760b4570620c3b183c59e7f78fdc5
unknown <pranauv1803@gmail.com>
2024-05-02 08:27:49 +0530

    changes in testing and workflow

--- server/server.js
+++ server/server.js
@@ -267,14 +267,4 @@ app.use((err, req, res, next) => {
   res.status(500).send("Error here!");
 });
 
-// comment this once testing is done
-// mongoose.connect(
-//   "mongodb+srv://lancify:1CeOEWH8wfnKgWVU@cluster0.hripjgl.mongodb.net/Lancify?retryWrites=true&w=majority",
-//   () => {
-//     app.listen(5500, () => {
-//       console.log("connected to mongodb and server started in port 5500");
-//     });
-//   }
-// );
-
 module.exports = app;
---
+++ server/test_server.test.data.js
@@ -0,0 +1,7 @@
+const users = [
+    { username: 'quote1', email: 'author1@gmail.com', password: "pwd2", user_id: "some_user_id", user_type: "user" },
+    { username: 'quote2', email: 'author2@gmail.com', password: "pwd2", user_id: "some_user_id", user_type: "seller" },
+    { username: 'quote3', email: 'author3@gmail.com', password: "pwd2", user_id: "some_user_id",  user_type: "admin" },
+  ]
+  
+  module.exports = users
